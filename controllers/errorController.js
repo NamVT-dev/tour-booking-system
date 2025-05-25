942664cf051c2ca338ae5364a678ec10754ad71f
NamVT <namvtfs1134@gmail.com>
2025-05-26 01:01:38 +0700

    fix error language

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,29 +1,29 @@
 const AppError = require("./../utils/appError");
 
 const handleCastErrorDB = (err) => {
-  const message = `Invalid ${err.path}: ${err.value}.`;
+  const message = `${err.path} không hợp lệ: ${err.value}.`;
   return new AppError(message, 400);
 };
 
 const handleDuplicateFieldsDB = (err) => {
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
 
-  const message = `Duplicate field value: ${value}. Please use another value!`;
+  const message = `Dữ liệu trùng lặp: ${value}. Hãy thử lại!`;
   return new AppError(message, 400);
 };
 
 const handleValidationErrorDB = (err) => {
   const errors = Object.values(err.errors).map((el) => el.message);
 
-  const message = `Invalid input data. ${errors.join(". ")}`;
+  const message = `Dữ liệu không hợp lệ. ${errors.join(". ")}`;
   return new AppError(message, 400);
 };
 
 const handleJWTError = () =>
-  new AppError("Invalid token. Please log in again!", 401);
+  new AppError("Token không hợp lệ. Hãy đăng nhập lại!", 401);
 
 const handleJWTExpiredError = () =>
-  new AppError("Your token has expired! Please log in again.", 401);
+  new AppError("Token hết hạn! Hãy đăng nhập lại.", 401);
 
 const sendError = (err, req, res) =>
   res.status(err.statusCode).json({
